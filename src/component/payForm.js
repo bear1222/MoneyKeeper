fdd78eef972fac1fcf6429a62667584b8b3a8291
bear1222 <doo1222.tw@gmail.com>
2024-09-02 19:50:05 +0800

    add concept and make some of payForm

--- src/component/payForm.js
+++ src/component/payForm.js
@@ -1,13 +1,50 @@
+import { MenuItem, Select, TextField, Typography, InputLabel } from '@mui/material';
 import React from 'react';
 
-export class PayForm extends React.Component {
-    constructor(props) {
-        super(props);
-    }
 
-    render() {
-        return (
-            <h1>Pay</h1>
-        );
-    }
+export function PayForm() {
+
+    const [age, setAge] = React.useState('');
+
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    };
+
+    return (
+        <>
+            <Typography
+            >
+                付款
+            </Typography>
+            <TextField
+            >
+                商品名稱
+            </TextField>
+            <TextField
+            >
+                金額
+            </TextField>
+            <InputLabel id="demo-simple-select-label">Age</InputLabel>
+            <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={age}
+                label="Age"
+                onChange={handleChange}
+            >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+            {/* <Select> */}
+            {/* <MenuItem value={}></MenuItem> */}
+            {/* <MenuItem value={}></MenuItem> */}
+            {/* <MenuItem value={}></MenuItem> */}
+            {/* <MenuItem value={}></MenuItem> */}
+            {/* <MenuItem value={}></MenuItem> */}
+            {/* <MenuItem value={}></MenuItem> */}
+            {/* <MenuItem value={}></MenuItem> */}
+            {/* </Select> */}
+        </>
+    );
 }
